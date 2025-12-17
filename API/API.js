810a4f00bf36065e6feab54aebe0c6891b5bd73d
XusanYarashov <xusanyarashov1@gmail.com>
2025-12-17 23:26:39 +0500

    fixed

--- API/API.js
+++ API/API.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { toast } from "react-toastify";
+
+console.log(import.meta.env.VITE_API_URL);
 
 export const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
